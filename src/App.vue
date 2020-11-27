3d11c8963d15f602c28bf73253c9b048ed27ae21
yuangungun <1091150407@qq.com>
2020-11-27 16:19:22 +0800

    feat:结构搭建、配置路由

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
-<template></template>
+<template>
+  <div>
+    <Header></Header>
+    <Router-view></Router-view>
+    <Footer></Footer>
+  </div>
+</template>
 
 <script>
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 export default {
   name: "App",
+  components: {
+    Header,
+    Footer,
+  },
 };
 </script>
 
-<style>
+<style lang="less" scoped>
 </style>
---
+++ src/components/Header/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="container">
+      <p>Heeder...</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style lang="less" scoped>
+.container{
+  background-color: cadetblue;
+  p{
+    color:darksalmon
+  }
+}
+</style>
